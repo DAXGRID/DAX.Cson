913d6a76346bd66a36134850dc9d86d26a89cb7b
Mogens Heller Grabe <mookid8000@gmail.com>
2017-09-27 16:31:46 +0200

    add test

---
+++ DAX.Cson.Tests/Bugs/TestRoundtripOddity.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using DAX.Cson.Tests.Extensions;
+using DAX.Cson.Tests.Stubs;
+using NUnit.Framework;
+
+namespace DAX.Cson.Tests.Bugs
+{
+    [TestFixture]
+    public class TestRoundtripOddity : FixtureBase
+    {
+        [TestCase(100)]
+        public void ProvokeError(int tests)
+        {
+            var serializer = new CsonSerializer();
+
+            foreach (var obj in new CimObjectFactory().Read().Take(tests))
+            {
+                var originalJson = obj.ToJson();
+                var roundtrippedJson = serializer.DeserializeObject(serializer.SerializeObject(obj)).ToJson();
+
+                if (string.Equals(originalJson, roundtrippedJson)) continue;
+
+                throw new AssertionException($@"The roundtripped JSON
+
+{roundtrippedJson.PrettifyJson()}
+
+does not match the original JSON
+
+{originalJson.PrettifyJson()}
+
+Here are the REAL objects:
+
+{obj.ToJson()}
+{roundtrippedJson}");
+            }
+        }
+    }
+}

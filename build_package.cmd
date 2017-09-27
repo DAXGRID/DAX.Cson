25e0472a96ec6012198229b1180d191233ad49b5
Mogens Heller Grabe <mookid8000@gmail.com>
2017-09-27 15:22:44 +0200

    update script

--- build_package.cmd
+++ build_package.cmd
@@ -8,8 +8,8 @@ set destination=%root%\deploy
 set msbuild=%ProgramFiles(x86)%\MSBuild\14.0\Bin\MsBuild.exe
 set nuget=%root%\tools\NuGet\nuget.exe
 
-set solutionfile=%root%\NRGi.Cson.sln
-set nuspec=%currentdir%\NRGi.Cson\NRGi.Cson.nuspec
+set solutionfile=%root%\DAX.Cson.sln
+set nuspec=%currentdir%\DAX.Cson\DAX.Cson.nuspec
 
 if "%version%"=="" (
 	echo Please remember to specify which version to build as an argument to this script.

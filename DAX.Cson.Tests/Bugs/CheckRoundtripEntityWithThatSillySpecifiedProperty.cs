969b5ce91a563ce09c6b0f47383ef9eb7d7768a1
Mogens Heller Grabe <mookid8000@gmail.com>
2018-06-29 10:42:46 +0200

    verify roundtrippable *Specified field

---
+++ DAX.Cson.Tests/Bugs/CheckRoundtripEntityWithThatSillySpecifiedProperty.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using DAX.CIM.PhysicalNetworkModel;
+using DAX.Cson.Tests.Extensions;
+using NUnit.Framework;
+
+namespace DAX.Cson.Tests.Bugs
+{
+    [TestFixture]
+    public class CheckRoundtripEntityWithThatSillySpecifiedProperty
+    {
+        [Test]
+        public void CanDoIt()
+        {
+            var coil = new PetersenCoil
+            {
+                mRID = "known-id",
+                aggregate = true,
+                aggregateSpecified = true
+            };
+
+            var serializer = new CsonSerializer();
+            
+            var cson = serializer.SerializeObject(coil);
+            var roundtrippedCoil = (PetersenCoil)serializer.DeserializeObject(cson);
+
+            var coilJson = coil.ToJson();
+            var roundtrippedCoilJson = roundtrippedCoil.ToJson();
+
+            Console.WriteLine($@"OK - this Petersen coil
+
+{coilJson.PrettifyJson()}
+
+got turned into this Petersen coil
+
+{roundtrippedCoilJson.PrettifyJson()}
+
+when roundtripped");
+
+            Assert.That(roundtrippedCoilJson, Is.EqualTo(coilJson));
+        }
+    }
+}
